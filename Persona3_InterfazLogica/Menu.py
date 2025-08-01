a58985eabd504b1c64d9d4ac8565c089533dcaf9
SaithC19 <saith.cajamarca@epn.edu.ec>
2025-07-31 21:55:09 -0500

    Create Menu.py

---
+++ Persona3_InterfazLogica/Menu.py
@@ -0,0 +1,13 @@
+def menu_admin(grafo):
+    while True:
+        print("\n--- Menú Administrador ---")
+        print("1. Ver rutas y conexiones")
+        print("2. Explorar lugares (BFS)")
+        print("3. Agregar ruta/punto turístico")
+        print("4. Eliminar ruta")
+        print("5. Actualizar ruta")
+        print("6. Guardar cambios en archivo")
+        print("7. Salir")
+        opcion = input("Opción: ")
+
+        # ... [resto de la función]
